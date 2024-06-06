0937c08c3bd5730fbefa20c6629b5b882c36ac3b
Paola Murguia <paolamurguia@gmail.com>
2024-06-05 20:21:53 -0400

    for, while, do-while

---
+++ Bucles/For.js
@@ -0,0 +1,41 @@
+// console.log("iniciando");
+
+// for (let index = 0; index <= 12; index++) {
+//   console.log(index);
+// }
+
+// for (let i = 0; i <= 12; i++) {
+//   console.log("#".repeat(i));
+// }
+
+// let dis= 12;
+// for (let i = 0; i <= 12; i++) {
+//    dis--;
+//   console.log(" ".repeat(dis)+"#".repeat(i));
+// }
+// let nombre = prompt("Cual es tu nombre?");
+// let apellido = prompt("Cual es tu apellido?")
+// let result= " ";
+// let nombreCompleto= nombre.toUpperCase()+" " +apellido.toUpperCase(); //converte a mayuscula
+// const ArrayName = nombreCompleto.split(" "); //"" guarda caracter por caracter " "guarda palabra por palabra
+// console.log(ArrayName);
+// resul = ArrayName[0][0] + ArrayName[1][0];
+// console.log(resul);
+
+
+//-----------------------------------WHILE------------------------------------------------
+
+let i=0;
+while(i<=10){//mientas 
+    console.log(i);
+    i++;
+}
+
+
+//--------------------------------DO-WHILE-------------------------------------------
+
+let num=0;
+do{
+    console.log(num);  //se ejecuta almenos una vez, nos sirve para convalidar algo ej: los prompt
+    num++;
+}while(num<10) //hacer mientas
